71513124d5df4a4668ea410faafcedbe9a531fa6
mariam-farrukh <50977034+mariam-farrukh@users.noreply.github.com>
2019-09-03 01:16:11 -0500

    Get request to users

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const server = express();
 server.use(express.json());
 
 server.post('/api/users', (req, res)=> {
-    const { name, bio} = req.body;
+    const {name, bio} = req.body;
     if( !name || !bio){
         res.status(400).json({ errorMessage: "Please provide name and bio for the user." });
 
@@ -18,7 +18,17 @@ server.post('/api/users', (req, res)=> {
                 res.status(500).json({ error: "There was an error while saving the user to the database" });
             });
     }
-})
+});
+
+server.get('/api/users', (req, res) => {
+    Users.find()
+        .then(user => {
+            res.status(200).json(user);
+        })
+        .catch(() => {
+            res.status(500).json({ error: "The users information could not be retrieved." })
+        });
+});
 
 const port = 5000;
 server.listen(port, ()=> console.log(`\n API on http://localhost:${port} \n`));
