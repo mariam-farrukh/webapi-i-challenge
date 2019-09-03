de5b1ca8c00e5535ffb6fb485cfb513a23fdfa2f
mariam-farrukh <50977034+mariam-farrukh@users.noreply.github.com>
2019-09-03 01:10:25 -0500

    Post request created (tested with Insomnia)

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 const express = require('express');
-const server = express();
+const Users = require('./data/db');
 
+const server = express();
 server.use(express.json());
 
+server.post('/api/users', (req, res)=> {
+    const { name, bio} = req.body;
+    if( !name || !bio){
+        res.status(400).json({ errorMessage: "Please provide name and bio for the user." });
+
+    } else {
+        Users.insert(req.body)
+            .then(user => {
+                res.status(201).json(user);
+            })
+            .catch(()=> {
+                res.status(500).json({ error: "There was an error while saving the user to the database" });
+            });
+    }
+})
+
 const port = 5000;
-server.listen(port, ()=> console.log(`\n API on https://localhost:${port} \n`));
+server.listen(port, ()=> console.log(`\n API on http://localhost:${port} \n`));
